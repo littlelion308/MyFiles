b113135559dee63fd05159b3f1ca4eaf122f5eb8
Little Lion <littlelion308@gmx.de>
2020-08-04 14:01:13 +0200

    fixed rofiunicode

--- .local/bin/rofiunicode.sh
+++ .local/bin/rofiunicode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-LINE=$(rofi -dmenu < $XDG_DATA_HOME/char)
+LINE=$(rofi -dmenu < ~/.local/share/char)
 CHOSEN=$(echo "$LINE" | cut -c1-1)
 echo "$CHOSEN" | tr -d '\n' | xclip -selection clipboard
 xdotool key shift+Insert
