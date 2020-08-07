5c04f5de9e398f3ae938c9069bb472d33470449f
Little Lion <littlelion308@gmx.de>
2020-08-07 13:18:03 +0200

    allowed better vim-fatigve intigration

--- .local/bin/scriptedit.sh
+++ .local/bin/scriptedit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ "$(printf 'script\nFile' | rofi -dmenu )" = 'script' ] && paha="$HOME/.local/bin" || paha="$HOME/Downloads"
+[ "$(printf 'script\nFile' | rofi -dmenu )" = 'script' ] && paha="$HOME/Downloads/Code/MyFiles/.local/bin" || paha="$HOME/Downloads"
 choosen="$(ls "$paha" --color=never | rofi -dmenu)"
 path="$paha"/"$choosen"
 [ "$paha" = 'script' ] && chmod +x "$path"
