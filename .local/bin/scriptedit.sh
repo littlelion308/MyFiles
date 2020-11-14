2e1d472db5e327d8cad40d4af59f863ae32e86a1
Little Lion <littlelion308@gmx.de>
2020-11-14 11:50:35 +0100

    cl

--- .local/bin/scriptedit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-[ "$(printf 'script\nFile' | rofi -dmenu )" = 'script' ] && paha="$HOME/Downloads/Code/MyFiles/.local/bin" || paha="$HOME/Downloads"
-choosen="$(ls "$paha" --color=never | rofi -dmenu)"
-path="$paha"/"$choosen"
-[ "$paha" = 'script' ] && chmod +x "$path"
-alacritty -e vi "$path"
--- .local/bin/vidconf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-bspc desktop -f '^11'
-alacritty -e pulsemixer&
-pavucontrol&
-bspc desktop -f '^10'
-alacritty -e htop&
-teams&
