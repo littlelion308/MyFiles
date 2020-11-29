8659638b8ea168dfe33553f93b859718da6fd535
Little Lion <littlelion308@gmx.de>
2020-11-29 21:38:44 +0100

    made it work with my current configuration

--- .local/bin/current-screen.sh
+++ .local/bin/current-screen.sh
@@ -2,5 +2,8 @@
 SCREENS=$(xrandr | grep 'connected' | grep -v 'disconnected' | awk '{print $1}' | sort)
 SCRNUMBER=$(echo "$SCREENS" | wc -l)
 [ "$SCRNUMBER" = 1 ] && bspc monitor -d I II III IV V VI VII VIII IX X XI XII ||
-[ "$SCRNUMBER" = 2 ] && [ "$(xrandr | grep 'connected' | grep -v 'disconnected' | awk '{print $1}' | sort | tr '\n' ' ')" ] &&
-	bspc monitor VGA-0 -d I II III IV V VI && bspc monitor LVDS -d I II III IV V VI
+if  [ "$SCRNUMBER" = 2 ]; then
+	Exactmoddel="$(xrandr | grep 'connected' | grep -v 'disconnected' | awk '{print $1}' | sort | tr '\n' ' ')"
+	bspc monitor "$(echo $Exactmoddel  | awk '{print $1}')" -d I II III IV V VI && bspc monitor "$(echo $Exactmoddel | awk '{print $1}')" -d I II III IV V VI
+	[    "$Exactmoddel" = "HDMI-0 LVDS %" ] &&  xrandr --output VGA-0 --off --output LVDS --mode 1366x768 --pos 1920x312 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-1-0 --off --output HDMI-1-1 --off --output VGA-1-1 --off
+fi
