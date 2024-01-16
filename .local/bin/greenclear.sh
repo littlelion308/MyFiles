e8ead9ad4e79e6425ffdbe2e517938e4fe56bddd
Little Lion <littlelion308@gmx.de>
2024-01-16 17:37:04 +0100

    Deleted usless scripts and config files

--- .local/bin/greenclear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pkill greenclip
-greenclip clear
-sleep 1s
-greenclip daemon&
--- .local/bin/pkginfo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-PKGS=$(pacman -Qq"$1")
-PKG=$(echo "$PKGS" | fzf)
-pacman -Qi "$PKG"
-# echo "$PKGS" | fzf | pacman -Qi
